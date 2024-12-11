0fa294a916ba9b3a4d4466398b934b093d1ca539
Shaurya <83474425+Shauryae1337@users.noreply.github.com>
2024-12-11 15:00:56 +0530

    Update responder.py

--- responder.py
+++ responder.py
@@ -44,4 +44,4 @@ def ban_ip():
     return jsonify({"message": f"IP {ip} banned for {time} seconds."}), 200
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=5000)
+    app.run(host='0.0.0.0', port=8090)
